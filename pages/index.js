64cea12061c7d0ae0495095898eb34c75da1d885
mrinalkantidutta <119732997+mrinalkantidutta@users.noreply.github.com>
2022-12-08 14:05:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Hi, This is Mrinal, Welcome to my first project!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I will come back with more editing <code>pages/index.js</code>
         </p>
       </main>
 
