39a06d31e5bc051987e216008fc8bc09a6603711
mrinalkantidutta <119732997+mrinalkantidutta@users.noreply.github.com>
2022-12-09 12:56:57 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+document.getElementsByTagName('meta')["description"].content = "A site developed by an Engineer from electronics/instrument/electrical field";
+
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
 }
