0ee0adfa99175a596349e1a11e473ba2f2e282bb
mrinalkantidutta <119732997+mrinalkantidutta@users.noreply.github.com>
2023-04-14 08:54:14 +0000

    Adding button to download my CV

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,14 @@ export default function Home() {
 
       <main>
         <Header title="Hi, This is Mrinal, Welcome to my first project!" />
+        <button onclick="downloadPDFMKDCV()">Mrinal's CV</button>
+        function downloadPDFMKDCV() {
+  const pdfUrl = '/MKD_CV.pdf';
+  const a = document.createElement('a');
+  a.href = pdfUrl;
+  a.download = 'MKD_CV.pdf';
+  a.click();
+}
         <p className="description">
           I will come back with more editing with help of Supriyo and Rangan.
         </p>
