c90b9326fc4b36c9d13292c54a987ecd8764fa9b
mrinalkantidutta <119732997+mrinalkantidutta@users.noreply.github.com>
2023-04-25 04:35:32 +0000

    Downloand my CV button with help of new Bing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+function downloadPDFMKDCV() {
+  let pdfUrl = '/MKD_CV.pdf';
+  let a = document.createElement('a');
+  a.href = pdfUrl;
+  a.download = 'MKD_CV.pdf';
+  a.click();
+}
 export default function Home() {
   return (
     <div className="container">
@@ -9,17 +16,10 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+           
       <main>
-        <Header title="Hi, This is Mrinal, Welcome to my first project!" />
-        <button onclick="downloadPDFMKDCV()">Mrinal's CV</button>
-        function downloadPDFMKDCV() {
-  const pdfUrl = '/MKD_CV.pdf';
-  const a = document.createElement('a');
-  a.href = pdfUrl;
-  a.download = 'MKD_CV.pdf';
-  a.click();
-}
+              <Header title="Hi, This is Mrinal, Welcome to my first project!" />
+        <button onClick={downloadPDFMKDCV}>Download My Resume</button>
         <p className="description">
           I will come back with more editing with help of Supriyo and Rangan.
         </p>
